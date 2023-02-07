5dbe2ca22fbb997cd2df31bef39250d2c3c281da
yasima-csiro <yasima.kankanamge@csiro.au>
2023-02-07 14:39:02 +1100

    Add reload config and disable update for config based IP whitelisting

--- userdetails-plugin/grails-app/views/authorisedSystem/edit.gsp
+++ userdetails-plugin/grails-app/views/authorisedSystem/edit.gsp
@@ -44,10 +44,12 @@
 						<fieldset class="form">
 							<g:render template="form"/>
 						</fieldset>
-						<fieldset class="buttons">
-							<g:actionSubmit class="btn btn-primary save" action="update" value="${message(code: 'default.button.update.label', default: 'Update')}" />
-							<g:actionSubmit class="btn btn-danger delete" action="delete" value="${message(code: 'default.button.delete.label', default: 'Delete')}" formnovalidate="" onclick="return confirm('${message(code: 'default.button.delete.confirm.message', default: 'Are you sure?')}');" />
-						</fieldset>
+						<g:if test="${grailsApplication.config.getProperty('enable.IpEdit', boolean, true)}">
+							<fieldset class="buttons">
+								<g:actionSubmit class="btn btn-primary save" action="update" value="${message(code: 'default.button.update.label', default: 'Update')}" />
+								<g:actionSubmit class="btn btn-danger delete" action="delete" value="${message(code: 'default.button.delete.label', default: 'Delete')}" formnovalidate="" onclick="return confirm('${message(code: 'default.button.delete.confirm.message', default: 'Are you sure?')}');" />
+							</fieldset>
+						</g:if>
 					</g:form>
 				</div>
 			</div>
--- userdetails-plugin/src/main/groovy/au/org/ala/userdetails/ConfigAuthorisedSystemRepository.groovy
+++ userdetails-plugin/src/main/groovy/au/org/ala/userdetails/ConfigAuthorisedSystemRepository.groovy
@@ -2,9 +2,17 @@ package au.org.ala.userdetails
 
 import au.org.ala.users.AuthorisedSystemRecord
 import grails.core.GrailsApplication
+import grails.util.Environment
 import grails.web.servlet.mvc.GrailsParameterMap
+import groovy.util.logging.Slf4j
 import org.apache.commons.lang3.NotImplementedException
+import org.grails.config.PropertySourcesConfig
+import org.grails.config.yaml.YamlPropertySourceLoader
+import org.springframework.core.env.PropertySources
+import org.springframework.core.io.Resource
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 
+@Slf4j
 class ConfigAuthorisedSystemRepository implements IAuthorisedSystemRepository {
 
     GrailsApplication grailsApplication
@@ -21,6 +29,14 @@ class ConfigAuthorisedSystemRepository implements IAuthorisedSystemRepository {
         def list = []
         def count = 0
         def query = params.q as String
+        def reload = params.getBoolean("reload", false)
+        if(reload) {
+            try {
+                grailsApplication.getConfig().merge(getConfig())
+            } catch (Exception e) {
+                log.error "Unable to reload configuration. Please correct problem and try again: ${e}", e
+            }
+        }
         if (query) {
             def records = grailsApplication.config.getProperty('authorised.systems', List, [])
                     .collect{ it as AuthorisedSystemRecord }
@@ -57,5 +73,46 @@ class ConfigAuthorisedSystemRepository implements IAuthorisedSystemRepository {
     @Override
     Boolean delete(Long id) {
         throw new NotImplementedException()
+    }
+
+    private ConfigObject getConfig() {
+        def configLocations = grailsApplication.config.getProperty("grails.config.locations", List, [])
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver()
+        ConfigObject config = new ConfigObject()
+
+        configLocations.each { location ->
+            log.info "reloading config file: ${location}"
+            Resource resource = resolver.getResource(location)
+            InputStream stream = null
+
+            try {
+                stream = resource.getInputStream()
+                ConfigSlurper configSlurper = new ConfigSlurper(Environment.current.name)
+                def props = null
+
+                if (resource.filename.endsWith('.groovy')) {
+                    props = stream.text
+                } else if (resource.filename.endsWith('.properties')) {
+                    props = new Properties()
+                    props.load(stream)
+                } else if (resource.filename.endsWith('.yml')) {
+                    def mapPropertySource = new YamlPropertySourceLoader().load( "yml config", resource, null )
+                    def sources = mapPropertySource.getIndices().collect() {mapPropertySource.get(it).getSource() }
+                    props = new Properties()
+                    sources.each {
+                        props << new PropertySourcesConfig(it).toProperties()
+                    }
+                }
+
+                if (props) {
+                    config.merge(configSlurper.parse(props))
+                }
+            } catch (Exception ex) {
+                log.warn ex.message
+            } finally {
+                stream?.close()
+            }
+        }
+        config
     }
 }

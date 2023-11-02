e7f300d6ff680afa4504a83569d0233460abc798
Mats Bovin <mats.bovin@gmail.com>
2023-11-02 09:53:27 +0100

    Revert "Allow authorized systems access"
    This reverts commit 713b87eec36cd5687652fd2ee97bbf3de18c05ea.

--- grails-app/controllers/au/org/ala/userdetails/UserDetailsWebServicesInterceptor.groovy
+++ grails-app/controllers/au/org/ala/userdetails/UserDetailsWebServicesInterceptor.groovy
@@ -30,8 +30,7 @@ class UserDetailsWebServicesInterceptor {
     }
 
     boolean before() {
-        if (!authorisedSystemService.isAuthorisedRequest(request, response, null, 'users/read') &&
-                !authorisedSystemService.isAuthorisedSystem(request)) {
+        if (!authorisedSystemService.isAuthorisedRequest(request, response, null, 'users/read')) {
             log.warn("Denying access to $actionName from remote addr: ${request.remoteAddr}, remote host: ${request.remoteHost}")
             response.sendError(HttpStatus.SC_UNAUTHORIZED)
 
